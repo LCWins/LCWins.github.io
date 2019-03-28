2cf4f4a5b915317739eed1fd3e5e08a82fe7030e
LCWins <1306959260@qq.com>
2019-03-28 14:46:56 +0800

    Site updated: 2019-03-28 14:46:50

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@
 
         var bannerNode = $('.top-image');
         if(bannerNode.data('enable')){
-            var index = parseInt((Math.random() * 12) + 1);
+            var index = parseInt((Math.random() * 13) + 1);
             bannerNode.attr('style','background-image:url(/banner/'+index+'.jpg)');
         }
     })
